ef3f8aabcc58b632835f498e38ad01f65aaed04d
devmunro <defmunro@gmail.com>
2024-06-06 08:23:55 +0100

    Add string concatenation functions

---
+++ Stringpractice/concat.js
@@ -0,0 +1,18 @@
+// ### Concatenate two string
+/* 06/06/2024
+Day 1
+*/
+
+
+function attempt1(s1,s2) {
+
+    return s1 + s2
+}
+
+console.log(attempt1("hello", "world"))
+
+function attempt2(s1,s2) { 
+
+    return s1 + "" + s2
+}
+
