d4aff03fc7694050b548afa73c5f7946f4a17090
devmunro <defmunro@gmail.com>
2024-06-06 08:33:23 +0100

    refactor: Improve string concatenation functions for readability and performance

--- Stringpractice/concat.js
+++ Stringpractice/concat.js
@@ -20,8 +20,33 @@ function attempt3(s1,s2) {
  return s1 + " " + s2
 
 }
-console.log(attempt3("hello", "world"))
+// console.log(attempt3("hello", "world"))
 
 
 // practice complete
 
+// ### Incorrect Function to reverse a string
+function reverseString(s) {
+    let reversed = "";
+    for (let i = 0; i < s.length; i++) {
+        reversed = s[i] + reversed;
+    }
+    return reversed; // added this part
+}
+
+// Test the function
+// console.log(reverseString("Hello"));  // Expected Output: "olleH"
+
+
+//Testing it out
+
+function reverseString2(s) {
+    let reversed = "";
+    for (let i = 0; i < s.length; i++) {
+        reversed = s[i] + reversed;
+    }
+    return reversed; // added this part
+}
+
+// Test the function
+console.log(reverseString2("olleh"));  // Expected Output: "olleH"
