10fe47c895a6dfd1a39e7c9375bd8c21663be52d
devmunro <defmunro@gmail.com>
2024-06-11 08:51:35 +0100

    initial commit

---
+++ Leetcodeattempts/3146.js
@@ -0,0 +1,21 @@
+/*3146. Permutation Difference between Two Strings
+Easy
+Topics
+Companies
+Hint
+You are given two strings s and t such that every character occurs at most once in s and t is a permutation of s.
+
+The permutation difference between s and t is defined as the sum of the absolute difference between the index of the occurrence of each character in s and the index of the occurrence of the same character in t.
+
+Return the permutation difference between s and t.*/
+
+//### Question ###
+var findPermutationDifference = function(s, t) {
+    
+};
+
+//###answer###
+
+var findPermutationDifference = function(s, t) {
+    
+};
